ce4d23be43e1785da650c3936dc18e8fbb39c075
coolmeen <emilyamor@post.bgu.ac.il/em>
2014-11-19 18:21:32 +0200

    hello

--- compiler.scm
+++ compiler.scm
@@ -9,51 +9,66 @@
 			(char? x)
 			(number? x)
 			(string? x)
-			(^quote? x)))
+			))
 
-(define (^quote? x)
-	(and (list? x)(eq? 'quote (car x))))
-
-(define (^var? x))				;TODO
+;(define (^var? x))				;TODO
 
 ;;;;;;;;;;;;;;;;;;
 ;; conditionals ;;
 ;;;;;;;;;;;;;;;;;;
-(define (^conditional? x)
-	(or 	(^if2? x)
-			(^if3? x)))
+; (define (^conditional? x)
+; 	(or 	(^if2? x)
+; 			(^if3? x)))
 
-(define (^if2? x))				;TODO
-(define (^if3? x))				;TODO
+; ;(define (^if2? x))				;TODO
+; ;(define (^if3? x))				;TODO
 
-;;;;;;;;;;;;
-;; lambda ;;
-;;;;;;;;;;;;
-(define (^lambda? x)
-	(or 	(^reg-lambda? x)
-			(^lambda-opt? x)
-			(^lambda-variadic? x)))
+; ;;;;;;;;;;;;
+; ;; lambda ;;
+; ;;;;;;;;;;;;
+; (define (^lambda? x)
+; 	(or 	(^reg-lambda? x)
+; 			(^lambda-opt? x)
+; 			(^lambda-variadic? x)))
+; ;
+; ;(define (^reg-lambda? x))		;TODO
+; ;(define (^lambda-opt? x))		;TODO
+; ;(define (^lambda-variadic? x))	;TODO
 
-(define (^reg-lambda? x))		;TODO
-(define (^lambda-opt? x))		;TODO
-(define (^lambda-variadic? x))	;TODO
+; ;;;;;;;;;;;;
+; ;; define ;;
+; ;;;;;;;;;;;;
+; (define (^define? x)
+; 	(or 	(^define-regular? x)
+; 			(^define-mit? x)))
 
-;;;;;;;;;;;;
-;; define ;;
-;;;;;;;;;;;;
-(define (^define? x)
-	(or 	(^define-regular? x)
-			(^define-mit? x)))
+; (define (^define-regular? x))	;TODO
+; (define (^define-mit? x))		;TODO
 
-(define (^define-regular? x))	;TODO
-(define (^define-mit? x))		;TODO
+; ;;;;;;;;;;;;;;;;;
+; ;; application ;;
+; ;;;;;;;;;;;;;;;;;
+; (define (^application? x))		;TODO
 
-;;;;;;;;;;;;;;;;;
-;; application ;;
-;;;;;;;;;;;;;;;;;
-(define (^application? x))		;TODO
+; ;;;;;;;;;
+; ;; seq ;;
+; ;;;;;;;;;
+; (define (^seq? x))				;TODO
 
-;;;;;;;;;
-;; seq ;;
-;;;;;;;;;
-(define (^seq? x))				;TODO
+
+(define parse
+  (let ((run
+	 (compose-patterns
+	  (pattern-rule
+	   `(,(? 'c ^const?))
+	   (lambda (c) `(const ,c)))
+	  (pattern-rule
+	   `(quote ,(? 'c))
+	   (lambda (c) `(const ,c)))
+	
+	  )))
+    (lambda (e)
+      (run e
+	   (lambda ()
+	     (error 'parse
+		    (format "I can't recognize this: ~s" e)))))))
