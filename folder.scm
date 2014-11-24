9544d25c49d8dcb8faae7b845c312e05b48ec643
hagai-lvi <hagai.lvi@gmail.com>
2014-11-24 21:22:11 +0200

    added flatten

--- folder.scm
+++ folder.scm
@@ -6,10 +6,25 @@
 		(compose-patterns
 			(pattern-rule
 				`(+ . ,(? 'vars-list))
-				(lambda (vars-list) vars-list))
+				(lambda (vars-list) (apply + vars-list)))
+			(pattern-rule `(,(? 'const (trace-lambda const? (x) (^const? x) ))) (lambda (x) x))
 		)))
 	(lambda (e)
-		(run e
+		(let ((exp (if 	(list? e)
+				    			(map fold e)
+				    			e)))
+			(run e
 			(lambda ()
 				(error 'parse
-				(format "I can't recognize this: ~s" e)))))))
+				(format "I can't recognize this: ~s" e)))))
+		)))
+
+(define (flatten sym list)
+	(letrec ((f (lambda (list succ)
+					(cond ((null? list) (succ list))
+					      ((and (list? (car list)) (eqv? (car (car list)) sym)) (f (cdr list) (lambda (rest) (succ `(,@(cdar list) ,@rest )))))
+					      (else (f (cdr list) (lambda (rest) (succ `(,(car list) ,@rest)))))
+					  ))))
+		(f list (lambda(x) x)))
+
+	)
