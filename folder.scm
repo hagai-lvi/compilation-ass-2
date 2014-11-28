8846c67f2e834d91a3cc4c687b7fbb060112e91b
hagai-lvi <hagai.lvi@gmail.com>
2014-11-28 17:55:39 +0200

    added basic fold

--- folder.scm
+++ folder.scm
@@ -7,23 +7,26 @@
 			(pattern-rule
 				`(+ . ,(? `args))
 				(lambda (args)
-					)
-			)
+					(apply + args)))
+		(pattern-rule
+			(? 'exp )
+			id)
+
 		)))
 	(lambda (e)
-		(let ()
-			(run e
+		(let ((exp (if 	(list? e)
+		    			(map fold e)
+		    			e)))
+			(run exp
 			(lambda ()
 				(error 'parse
-				(format "I can't recognize this: ~s" e)))))
-		)))
+				(format "I can't recognize this: ~s" e))))))))
 
 (define (flatten sym list)
 	(letrec ((f (lambda (list succ)
 					(cond ((null? list) (succ list))
 					      ((and (list? (car list)) (eqv? (car (car list)) sym)) (f (cdr list) (lambda (rest) (succ `(,@(cdar list) ,@rest )))))
-					      (else (f (cdr list) (lambda (rest) (succ `(,(car list) ,@rest)))))
-					  ))))
+					      (else (f (cdr list) (lambda (rest) (succ `(,(car list) ,@rest)))))))))
 		(f list (lambda(x) x))))
 
 (define (quotify l)
@@ -34,5 +37,4 @@
 
 (define (part pred lst)
 	(cons 	(cons #t (filter pred lst))
-			(cons #f (filter (lambda (x) (not (pred x)) ) lst))
-	))
+			(cons #f (filter (lambda (x) (not (pred x)) ) lst))))
